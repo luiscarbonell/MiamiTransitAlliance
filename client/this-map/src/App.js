1614f13047b19787a8d8dd2a0a795d18214f9f3b
Whitney Lubin <whitneylubin@icloud.com>
2018-08-11 16:39:08 -0400

    days of week

--- client/this-map/src/App.js
+++ client/this-map/src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Map from './Map.js';
 import TimeSlider from './components/controller/TimeSlider';
-=======
-import Map from './Map.js'
 import Header from './Header.js'
->>>>>>> Header component for map
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
+
 		<TimeSlider />
-=======
+
         <Header />
->>>>>>> Header component for map
         <Map />
       </div>
     );
