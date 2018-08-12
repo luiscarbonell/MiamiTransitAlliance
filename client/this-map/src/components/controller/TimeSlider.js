55d4e900f98dd034f5b4a72e79fbb411148ea818
baamosk <bunth667@gmail.com>
2018-08-12 00:13:31 -0400

    added time slider

--- client/this-map/src/components/controller/TimeSlider.js
+++ client/this-map/src/components/controller/TimeSlider.js
@@ -2,11 +2,15 @@ import React, { Component }  from 'react';
 import './TimeSlider.css';
 
 class TimeSlider extends Component {
+    state = {
+        time: '9:00',
+        meridien: 'a.m.'        
+    };
 
 	render() {
         return (
                 <div className="slider-container">
-                    <div>SLIDER</div>
+                    <div>{this.state.time} {this.state.meridien}</div>
                 </div>
         );
 	}
